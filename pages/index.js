228e3c0335a77fc24514608799e070b40ae73199
ootsun <gabriel.delhaye@hotmail.com>
2022-02-11 10:14:54 +0100

    Add link to wenclaim.xyz

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>How much ?!!</title>
       </Head>

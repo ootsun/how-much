3d99542c0ae62662ebc96cd1148a217315c8b284
ootsun <gabriel.delhaye@hotmail.com>
2022-03-02 10:28:38 +0100

    Apply pagination on projects

--- components/projects/project-list.js
+++ components/projects/project-list.js
@@ -2,7 +2,7 @@ import {Logo} from './logo.js';
 import {useEffect, useMemo, useState} from 'react';
 import {deleteProject, findAll} from '../../lib/client/projectHandler.js';
 import ErrorModal from '../error-modal.js';
-import {useTable, useSortBy, useGlobalFilter} from 'react-table';
+import {useTable, useSortBy, useGlobalFilter, usePagination} from 'react-table';
 import {GlobalFilter} from '../global-filter.js';
 import {Toast} from '../toast.js';
 import {LoadingCircle} from '../loading-circle.js';
@@ -71,19 +71,26 @@ export function ProjectList({projects, selectedProject, setSelectedProject, upda
       }
     },
     useGlobalFilter,
-    useSortBy);
+    useSortBy,
+    usePagination);
 
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page,
+    previousPage,
+    nextPage,
+    canPreviousPage,
+    canNextPage,
+    gotoPage,
+    pageCount,
     prepareRow,
     state,
     setGlobalFilter
   } = tableInstance;
 
-  const {globalFilter} = state;
+  const {globalFilter, pageIndex} = state;
 
   async function refreshList() {
     try {
@@ -117,6 +124,8 @@ export function ProjectList({projects, selectedProject, setSelectedProject, upda
     }
   }
 
+  console.log(canPreviousPage, canNextPage, pageIndex)
+
   return (
     <div className="flex flex-col">
       <Toast message={toastMessage} setMessage={setToastMessage}/>
@@ -139,14 +148,14 @@ export function ProjectList({projects, selectedProject, setSelectedProject, upda
                             className="py-3 px-6 font-medium tracking-wider text-left text-gray-700 dark:text-gray-400 w-1/3">
                           {column.render('Header')}{column.isSorted ? column.isSortedDesc ?
                           <span>
-                            <svg className="w-3 h-3 inline ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            <svg className="w-3 h-3 inline ml-1 text-orange-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                  xmlns="http://www.w3.org/2000/svg">
                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                     d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
                             </svg>
                           </span> :
                           <span>
-                            <svg className="w-3 h-3 inline ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            <svg className="w-3 h-3 inline ml-1 text-orange-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                  xmlns="http://www.w3.org/2000/svg">
                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                     d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
@@ -160,7 +169,7 @@ export function ProjectList({projects, selectedProject, setSelectedProject, upda
               <tbody {...getTableBodyProps()}
                      className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
               {
-                rows.map(row => {
+                page.map(row => {
                   prepareRow(row)
                   return (
                     <tr {...row.getRowProps()} className="hover:bg-gray-100 dark:hover:bg-gray-700">
@@ -179,6 +188,42 @@ export function ProjectList({projects, selectedProject, setSelectedProject, upda
               </tbody>
             </table>
           </div>
+          <nav aria-label="Page navigation" className="flex justify-center m-1">
+            <ul className="inline-flex items-center -space-x-px">
+              <li>
+                <button disabled={!canPreviousPage} onClick={() => previousPage()}
+                   className="page-arrow-button page-arrow-button-previous">
+                  <span className="sr-only">Previous</span>
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd"
+                          d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                          clipRule="evenodd"></path>
+                  </svg>
+                </button>
+              </li>
+              {
+                [...Array(pageCount)].map((x, i) =>
+                  <li>
+                    <button disabled={pageIndex === i} onClick={() => gotoPage(i)}
+                      className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:text-orange-500 disabled:hover:dark:bg-gray-800 disabled:hover:bg-white">
+                      {i + 1}
+                    </button>
+                  </li>
+                )
+              }
+              <li>
+                <button disabled={!canNextPage} onClick={() => nextPage()}
+                        className="page-arrow-button page-arrow-button-next">
+                  <span className="sr-only">Next</span>
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clipRule="evenodd"></path>
+                  </svg>
+                </button>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>

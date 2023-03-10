0eab86f802873edda873c1a6562168c673ea90f1
OoTsun <gabriel.delhaye@hotmail.com>
2023-03-10 22:43:53 +0100

    Install flowbite-react

--- next.config.js
+++ next.config.js
@@ -3,4 +3,5 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  transpilePackages: ['react-icons'],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/flowbite/**/*.js'
+    './node_modules/flowbite-react/**/*.js',
   ],
   darkMode: 'media',
   theme: {

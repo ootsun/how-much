cc79f8896426222e008a33758c693319b5b96175
OoTsun <gabriel.delhaye@hotmail.com>
2023-03-11 00:06:55 +0100

    Use NextJS's Image tag instead of img for logos

--- components/projects/logo.js
+++ components/projects/logo.js
@@ -1,9 +1,14 @@
+import Image from "next/image.js";
+
 export function Logo({url, alt}) {
 
   return (
-      <img
-        src={url}
-        alt={alt}
-        className="inline w-5 h-5 relative inline-block"/>
+    <Image
+      src={url}
+      alt={alt}
+      className="inline relative inline-block"
+      width={20}
+      height={20}
+    />
   );
 }

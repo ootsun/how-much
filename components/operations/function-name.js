1e1e67d9bf1f06d65871fbcedb17ad61ef3b6652
OoTsun <gabriel.delhaye@hotmail.com>
2023-04-11 22:19:26 +0200

    Fix: formatCamelCaseToWords handle multiple following UPPERCASE letters like "depositETH"

--- components/operations/function-name.js
+++ components/operations/function-name.js
@@ -8,8 +8,11 @@ export function FunctionName({name, tooltip = false}) {
 
   const formatCamelCaseToWords = (str) => {
     // Split the string into two parts at the capital letter, lowercase letter, or digit
-    const regex = /([a-z0-9]+|[A-Z][a-z0-9]*)/g;
+    const regex = /([a-z0-9]+|[A-Z0-9]+(?![a-z])|[A-Z0-9][a-z0-9]*)/g;
     const parts = str.match(regex);
+    if(str == 'addLiquidityETH') {
+      console.log(parts);
+    }
 
     // If there is only one part, return it as-is
     if (parts.length === 1) {
@@ -18,12 +21,7 @@ export function FunctionName({name, tooltip = false}) {
 
     // Capitalize the first letter of the first word and join the parts with a space in between
     parts[0] = parts[0].charAt(0).toUpperCase() + parts[0].slice(1);
-    const formattedParts = parts.map(part => {
-      if (part.match(/^[A-Z]+$/)) {
-        return part;
-      }
-      return part.charAt(0).toUpperCase() + part.slice(1).toLowerCase();
-    });
+    const formattedParts = parts.map(part => part.charAt(0).toUpperCase() + part.slice(1));
     return formattedParts.join(' ');
   }
 

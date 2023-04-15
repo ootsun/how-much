39eed2ccb08cd5201ecd5ec0a88a49f8daeb7b22
OoTsun <gabriel.delhaye@hotmail.com>
2023-04-15 13:34:53 +0200

    Simplify formatCamelCaseToWords function

--- components/operations/function-name.js
+++ components/operations/function-name.js
@@ -7,17 +7,10 @@ export function FunctionName({name, tooltip = false}) {
   const isMobileDisplay = useMobileDisplayHook();
 
   const formatCamelCaseToWords = (str) => {
-    // Split the string into two parts at the capital letter, lowercase letter, or digit
     const regex = /([a-z0-9]+|[A-Z0-9]+(?![a-z])|[A-Z0-9][a-z0-9]*)/g;
     const parts = str.match(regex);
 
-    // If there is only one part, return it as-is
-    if (parts.length === 1) {
-      return capitalizeFirstLetter(parts[0]);
-    }
-
-    // Capitalize the first letter of the first word and join the parts with a space in between
-    parts[0] = capitalizeFirstLetter(parts[0]);
+    // Capitalize the first letter and join the parts with a space in between
     const formattedParts = parts.map(part => capitalizeFirstLetter(part));
     return formattedParts.join(' ');
   }
